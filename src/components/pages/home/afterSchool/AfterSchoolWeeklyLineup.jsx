e16795227c40c9ade9868dc685857872405b043b
musfiqurofficial <musfiqurofficial1999@gmail.com>
2023-03-11 11:07:57 +0600

    after school page

---
+++ src/components/pages/home/afterSchool/AfterSchoolWeeklyLineup.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const AfterSchoolWeeklyLineup = () => {
+  return (
+    <div>
+      <div className="bg-[#fff] my-20">
+        <div className="px-4 mx-auto max-w-full md:max-w-full lg:max-w-screen-lg md:px-18 lg:px-8">
+          <h1 className="text-center font-[400] text-[36px]">
+            Weekly Lineup: Core Daily Focuses!
+          </h1>
+          <h3 className="text-center font-[300] text-[20px] mt-3 mb-8">
+            Exact activity for each day may vary depending on interest and
+            weather
+          </h3>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 items-center">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AfterSchoolWeeklyLineup;
