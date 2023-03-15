d924cc547fbf27e1ebbd04ce611653bfd45ad256
Mamun2k22 <mamun180037@gmail.com>
2023-03-16 04:48:12 +0600

    responsive school card

--- src/components/pages/afterSchool/afterSchoolWeekly/AfterSchoolWeeklyLineup.jsx
+++ src/components/pages/afterSchool/afterSchoolWeekly/AfterSchoolWeeklyLineup.jsx
@@ -20,164 +20,153 @@ const AfterSchoolWeeklyLineup = () => {
         </h3>
 
         {/* Monday */}
-        <div className="grid grid-cols-2">
-          {/* <h1 className=' text-[30px] font-medium'>- Monday -</h1> */}
-
+        <div className="grid grid-cols-1 sm:grid-cols-2 my-20">
           <div className="flex flex-col gap-y-6 items-center">
-            <h2 className="text-end text-[32px] font-semibold drop-shadow-xl ml-60">
+            <h2 className="text-center sm:text-end text-[32px] font-semibold drop-shadow-xl sm:ml-60">
               - Monday -
             </h2>
-            <p className="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none w-full sm:w-9/12 px-7 ml-56 text-center text-base">
+            <p className="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none sm:w-9/12 px-5 sm:px-10 sm:ml-56 text-center text-base">
               <span className="font-medium text-[20px]">
                 Team Sports & Playground* 3:45-4:45p
               </span>{" "}
-              <br />
-              At the 5 acre Preston Athletic Fields and Park <br /> across the
-              street.
+
+              At the 5 acre Preston Athletic Fields and Park
+              across the street.
             </p>
 
-            <div className=" ml-52 p-2 shadow-md shadow-slate-300">
+            <div className="ml-4 sm:ml-52 p-2 shadow-md shadow-slate-300">
               <img
-                className="mb-4 w-[338px] h-[425px] "
+                className="mb-4 w-[338px] h-[425px] sm:w-full sm:h-full object-cover"
+                src={mon1}
+                alt=""
+                srcset=""
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-y-6 mt-10 sm:mt-0">
+            <div className="mx-auto sm:mx-4 p-2 sm:w-[338px] sm:h-[425px] shadow-md shadow-slate-300">
+              <img
+                className="mb-10"
                 src={mon2}
                 alt=""
                 srcset=""
               />
-            </div>
-          </div>
-
-          <div className="flex flex-col gap-y-6">
-            <div className="ml-4 p-2 pb-5 shadow-md shadow-slate-300 w-[338px]  h-[425px] ">
-              <img className="mb-4" src={mon1} alt="" srcset="" />
               <p className="text-center">*Weather Depending</p>
             </div>
 
-            <p className="-z-10 -ml-8 mt-10 border-4 rounded-full border-[#4EBA59] py-4 rounded-l-none w-9/12  px-6 mr-64 text-center text-base">
-              <span className="font-medium text-[20px]">
+            <p className="-z-10 -ml-8 mt-10 border-4 rounded-full border-[#4EBA59] py-5 rounded-l-none sm:rounded-l-none sm:w-9/12 sm:px-10 text-center text-base">
+              <span className="text-2xl md:text-2xl font-medium text-[20px]">
                 Alternative: Trampolines, Art, Free-play!
               </span>{" "}
               <br />
-              On-site in our playground area and in our <br /> classrooms!
+              On-site in our playground area and in our
             </p>
           </div>
         </div>
-        {/* Tuesday */}
-        <div className="grid grid-cols-2 my-20">
-          {/* <h1 className=' text-[30px] font-medium'>- Monday -</h1> */}
 
-          <div className="flex flex-col gap-y-6 items-center">
-            <h2 className="text-end text-[32px] font-semibold drop-shadow-xl ml-60">
+        {/* Tuesday */}
+        <div class="grid grid-cols-1 md:grid-cols-2 my-20">
+          <div class="flex flex-col gap-y-6 items-center">
+            <h2 class="text-end text-2xl md:text-4xl font-semibold drop-shadow-xl md:ml-60">
               - Tuesday -
             </h2>
-            <p className="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none w-9/12 px-5 ml-60 text-center text-base">
-              <span className="font-medium text-[20px]">
+            <p class="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none w-full md:w-9/12 px-5 md:px-7 md:ml-60 text-center text-base md:text-lg">
+              <span class="font-medium text-2xl md:text-2xl">
                 Film Club 3:45-4:45p
-              </span>{" "}
+              </span>
               <br />
-              How to make a film A-Z. Kids are grouped <br /> for
-              age-appropriate curriculum
+              How to make a film A-Z. Kids are grouped
+              <br />
+              for age-appropriate curriculum
             </p>
-
-            <div className="ml-52 p-2 shadow-md shadow-slate-300">
+            <div class="ml-4 md:ml-52 p-2 shadow-md shadow-slate-300">
               <img
-                className="mb-4 w-[338px] h-[425px] "
+                class="mb-4 w-full md:w-[338px] h-[425px] object-cover"
                 src={Tuesday1}
                 alt=""
-                srcset=""
               />
             </div>
           </div>
 
-          <div className="flex flex-col gap-y-6">
-            <div className="ml-4 p-2 w-[338px] h-[425px]  shadow-md shadow-slate-300">
-              <img className="mb-10" src={tuesday2} alt="" srcset="" />
-              <p className="text-center">*Weather Depending</p>
+          <div class="flex flex-col gap-y-6 mt-10 md:mt-0">
+            <div class="ml-4 p-2 md:w-[338px] h-[425px] shadow-md shadow-slate-300">
+              <img class="mb-10 w-full h-full object-cover" src={tuesday2} alt="" />
+              <p class="text-center">*Weather Depending</p>
             </div>
+            <p class="-z-10 -ml-10 border-4 rounded-full border-[#4EBA59] py-4 rounded-l-none w-full md:w-9/12 px-4 md:px-7 md:mr-64 text-center text-base md:text-lg lg:mt-10 mt-12">
+              <span class="font-medium text-2xl md:text-2xl">
+                Alternative: Trampolines, Art, Free-play! <br />
+              </span>
 
-            <p className="-z-10 -ml-8 mt-10 border-4 rounded-full border-[#4EBA59] py-3 rounded-l-none w-9/12  px-6 mr-64 text-center text-base">
-              <span className="font-medium text-[20px]">
-                Alternative: Trampolines, Art, Free-play!
-              </span>{" "}
-              <br />
-              On-site in our playground area and in our <br /> classrooms!
+              On-site in our playground area and in our
+
+              classrooms!
             </p>
           </div>
         </div>
+
 
         {/* Wednesday */}
-        <div className="grid grid-cols-2 my-20">
-          {/* <h1 className=' text-[30px] font-medium'>- Monday -</h1> */}
-
-          <div className="flex flex-col gap-y-6 items-center">
-            <h2 className="text-end text-[32px] font-semibold drop-shadow-xl ml-60">
+        <div class="grid grid-cols-1 md:grid-cols-2 my-20">
+          <div class="flex flex-col gap-y-6 items-center">
+            <h2 class="text-end text-2xl md:text-4xl font-semibold drop-shadow-xl md:ml-60">
               - Wednesday -
             </h2>
-            <p className="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none w-9/12 px-6 ml-56 text-center text-base">
-              <span className="font-medium text-[20px]">
-                Horseback Riding!* 2-3:30p & 3:45-5:15p
-              </span>{" "}
+            <p class="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none w-full md:w-9/12 px-5 md:px-7 md:ml-60 text-center text-base md:text-lg">
+              <span class="font-medium text-2xl md:text-3xl">
+                Film Club 3:45-4:45p
+              </span>
               <br />
-              On-site in our horse paddock & on the <br /> Preston-Fall City
-              Trail!
+              How to make a film A-Z. Kids are grouped
+              <br />
+              for age-appropriate curriculum
             </p>
-
-            <div className="ml-52 p-2 shadow-md shadow-slate-300">
+            <div class="ml-4 md:ml-52 p-2 shadow-md shadow-slate-300">
               <img
-                className="mb-4 w-[338px] h-[425px] "
-                src={wed2}
+                class="mb-4 w-full md:w-[338px] h-[425px] object-cover"
+                src={Tuesday1}
                 alt=""
-                srcset=""
               />
             </div>
           </div>
 
-          <div className="flex flex-col gap-y-6">
-            <div className="ml-4 p-2  w-[338px] h-[425px]  shadow-md shadow-slate-300">
-              <img className="mb-10" src={wed1} alt="" srcset="" />
-              <p className="text-center">*Weather Depending</p>
+          <div class="flex flex-col gap-y-6 mt-10 md:mt-0">
+            <div class="ml-4 p-2 md:w-[338px] h-[425px] shadow-md shadow-slate-300">
+              <img class="mb-10 w-full h-full object-cover" src={tuesday2} alt="" />
+              <p class="text-center">*Weather Depending</p>
             </div>
-
-            <p className="-z-10 -ml-8 mt-10  border-4 rounded-full border-[#4EBA59] py-4 rounded-l-none w-10/12  px-5 text-center text-base">
-              <span className="font-medium text-[20px]">
-                Optional**: Cognitive Ability Training
-              </span>{" "}
+            <p class="-z-10 lg:-ml-12 border-4 rounded-full border-[#4EBA59] py-3 rounded-l-none w-full md:w-9/12 px-6 md:px-7 md:mr-64 text-center text-base md:text-lg lg:mt-10 mt-12">
+              <span class="font-medium text-2xl md:text-3xl">
+                Alternative: Trampolines, Art, Free-play!
+              </span>
               <br />
-              Guaranteed to improve executive processing, processing <br />{" "}
-              speed, attention control, learning speed, and memory <br />{" "}
-              recall.{" "}
-              <a
-                className="text-blue-400 underline"
-                href="https://allstarkiddos.lpages.co/festival-school-interest-form/"
-              >
-                {" "}
-                Click here{" "}
-              </a>{" "}
-              to download the info packet.
+              On-site in our playground area and in our
+              <br />
+              classrooms!
             </p>
-            <p className="-m-5 text-center">** Extra Charge Applies</p>
           </div>
         </div>
-        {/* Thursday */}
-        <div className="grid grid-cols-2 my-20">
-          {/* <h1 className=' text-[30px] font-medium'>- Monday -</h1> */}
 
-          <div className="flex flex-col gap-y-6 items-center">
-            <h2 className="text-end text-[32px] font-semibold drop-shadow-xl ml-60">
+        {/* Thursday */}
+        <div class="grid grid-cols-1 md:grid-cols-2 my-20 gap-10">
+          <div class="flex flex-col gap-6 items-center">
+            <h2 class="text-end text-3xl font-semibold drop-shadow-xl md:ml-60">
               - Thursday -
             </h2>
-            <p className="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none w-10/12 px-5 ml-56 text-center text-base">
-              <span className="font-medium text-[20px]">
-                Nature Day with Horses!* 3:45-4:45p
+            <p class="border-4 rounded-full border-green-500 py-4 rounded-r-none w-full md:w-10/12 px-5 md:ml-64 sm:ml-64 text-center text-base">
+              <span class="font-medium lg:text-2xl text-xl">
+                Nature Day with Horses* 3:45-4:45p <br />
               </span>{" "}
-              <br />
+
               We take the horses on the Preston-Fall City Trail
-              <br /> and take turns hiking, horseback riding, and <br /> leading
+              and take turns hiking, horseback riding, & leading
               our miniature horse.
             </p>
 
-            <div className="ml-52 p-2 shadow-md shadow-slate-300">
+            <div class="ml-4 md:ml-52 p-2 shadow-md shadow-slate-300">
               <img
-                className="mb-4 w-[338px] h-[425px] "
+                class="mb-4 w-full md:w-[338px] h-auto md:h-[425px]"
                 src={wed2}
                 alt=""
                 srcset=""
@@ -185,36 +174,29 @@ const AfterSchoolWeeklyLineup = () => {
             </div>
           </div>
 
-          <div className="flex flex-col gap-y-6">
-            <div className="ml-4 p-2 w-[338px] h-[425px] shadow-md shadow-slate-300">
-              <img
-                className="mb-10"
-                src={wed1}
-                alt=""
-                srcset=""
-              />
-              <p className="text-center">*Weather Depending</p>
+          <div class="flex flex-col gap-6">
+            <div class="ml-4 p-2 w-full md:w-[338px] h-auto md:h-[425px] shadow-md shadow-slate-300">
+              <img class="mb-10" src={wed1} alt="" srcset="" />
+              <p class="text-center">*Weather Depending</p>
             </div>
 
-            <p className="-z-10 -ml-8 mt-10  border-4 rounded-full border-[#4EBA59] py-4 rounded-l-none w-9/12  px-6 mr-64 text-center text-base">
-              <span className="font-medium text-[20px]">
+            <p class="-z-10 -ml-16 mt-10 border-4 rounded-full border-green-500 py-4 rounded-l-none w-full md:w-9/12 px-6 md:mr-64 text-center text-base">
+              <span class="font-medium text-2xl">
                 Alternative: Movie + popcorn!
               </span>{" "}
               <br />
-              In our classrooms. Free-play and trampolines <br /> are also an
-              available!
+              In our classrooms. Free-play and trampolines  are also available!
             </p>
           </div>
         </div>
-        {/* Friday */}
-        <div className="grid grid-cols-2 my-20">
-          {/* <h1 className=' text-[30px] font-medium'>- Monday -</h1> */}
 
+        {/* Friday */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 my-20">
           <div className="flex flex-col gap-y-6 items-center">
-            <h2 className="text-end text-[32px] font-semibold drop-shadow-xl ml-60">
+            <h2 className="text-center sm:text-end text-[32px] font-semibold drop-shadow-xl sm:ml-60">
               - Friday -
             </h2>
-            <p className="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none w-9/12 px-5 ml-56 text-center text-base">
+            <p className="border-4 rounded-full border-[#4EBA59] py-4 rounded-r-none sm:w-9/12 px-5 sm:px-10 sm:ml-56 text-center text-base">
               <span className="font-medium text-[20px]">
                 Cooking/baking 2-3:30p & 3:45-5:15p
               </span>{" "}
@@ -223,9 +205,9 @@ const AfterSchoolWeeklyLineup = () => {
               <br /> and learn about nutrition by a Professional Chef!
             </p>
 
-            <div className="ml-52 p-2 shadow-md shadow-slate-300">
+            <div className="ml-4 sm:ml-52 p-2 shadow-md shadow-slate-300">
               <img
-                className="mb-4 w-[338px] h-[425px] "
+                className="mb-4 w-[338px] h-[425px] sm:w-full sm:h-full object-cover"
                 src={friday2}
                 alt=""
                 srcset=""
@@ -233,8 +215,8 @@ const AfterSchoolWeeklyLineup = () => {
             </div>
           </div>
 
-          <div className="flex flex-col gap-y-6">
-            <div className="ml-4 p-2 w-[338px] h-[425px] shadow-md shadow-slate-300">
+          <div className="flex flex-col gap-y-6 mt-10 sm:mt-0">
+            <div className="mx-auto sm:mx-4 p-2 sm:w-[338px] sm:h-[425px] shadow-md shadow-slate-300">
               <img
                 className="mb-10"
                 src={friday1}
@@ -244,16 +226,17 @@ const AfterSchoolWeeklyLineup = () => {
               <p className="text-center">*Weather Depending</p>
             </div>
 
-            <p className="-z-10 -ml-8 mt-10 border-4 rounded-full border-[#4EBA59] py-4 rounded-l-none w-8/12  px-5 text-center text-base">
+            <p className="-z-10 -ml-8 mt-10 border-4 rounded-full border-[#4EBA59] py-4 rounded-l-none sm:rounded-l-none sm:w-8/12 sm:px-10 text-center text-base">
               <span className="font-medium text-[20px]">
                 Alternative: Movie + popcorn!
               </span>{" "}
               <br />
-              In our classrooms. Free-play and <br /> trampolines are also an
-              option!
+              In our classrooms. Free-play and <br /> trampolines are also an option!
             </p>
           </div>
         </div>
+
+
       </div>
     </div>
   );
